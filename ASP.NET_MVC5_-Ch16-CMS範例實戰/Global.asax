a6735aefd6faefaed93941c5301fe0d4355e2c1f
joe lee <as955133@gmail.com>
2018-07-06 10:40:06 +0800

    加入專案檔案。

---
+++ ASP.NET_MVC5_-Ch16-CMS範例實戰/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC5__Ch16_CMS範例實戰.MvcApplication" Language="C#" %>
---
+++ ASP.NET_MVC5_-Ch16-CMS範例實戰/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5__Ch16_CMS範例實戰.Startup))]
+namespace ASP.NET_MVC5__Ch16_CMS範例實戰
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
